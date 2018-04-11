2f2e420bc4fb2f16cdfedd00a8d081298e8838ac
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2018-04-11 00:04:55 -0700

    created express server and todos POST handler

--- server/server.js
+++ server/server.js
@@ -1,16 +1,31 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+
 let mongoose = require('./db/mongoose.js').mongoose
 let User = require('./models/user').User
 let Todo = require('./models/todo').Todo
 
+let app = express()
 
+//parse json requests
+app.use(bodyParser.json())
 
-let newUser = new User({
-    email: 'arakeedgar@gmail.com'
+//post method on todos
+app.post('/todos', (req, res) => {
+    let todo = new Todo({
+        text: req.body.text
+    })
+    todo.save().then((doc) => {
+        console.log(doc)
+        res.status(200).send(doc)
+    }, (err) => {
+        res.status(400).send(err)
+        console.log(err)
+    })
 })
 
-newUser.save().then( (docs) => {
-    console.log(docs)
-    mongoose.disconnect()
-}, (err) => {
-    console.log(err)
+
+let port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server deployed on port ${port}`)
 })
