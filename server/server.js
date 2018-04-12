e25f69d3275eeda5aba6c4b20e0154891144fb46
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2018-04-12 00:52:13 -0700

    Added a GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,13 +4,14 @@ let bodyParser = require('body-parser')
 let mongoose = require('./db/mongoose.js').mongoose
 let User = require('./models/user').User
 let Todo = require('./models/todo').Todo
+let ObjectID = require('mongodb').ObjectID
 
 let app = express()
 
 //parse json requests
 app.use(bodyParser.json())
 
-//post method on todos saves a todo
+//POST /todos | saves todo in body to todos db
 app.post('/todos', (req, res) => {
     let todo = new Todo({
         text: req.body.text
@@ -24,14 +25,37 @@ app.post('/todos', (req, res) => {
     })
 })
 
-//get method on todos returns all todos
+//GET /todos | returns all todos
 app.get('/todos', (req, res) => {
+
     Todo.find().then((todos) => {
         res.status(200).send(todos)
     }, (err) => {
         res.status(400).send(err)
     })
 })
+
+//GET /todos/:id | returns todo of passed id
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id
+    if (!ObjectID.isValid(id)){
+        res.status(404).send('INVALID id')
+        return
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (todo === null){
+            res.status(404).send(`todo with id: ${id} does not exist`)
+            return
+        }
+
+        res.status(200).send(todo)
+    }).catch((err) => {
+        res.status(400).send('Error retrieving todo')
+    })
+})
+
+
 
 
 let port = process.env.PORT || 3000
