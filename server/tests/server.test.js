ddb55ef91db792ac50ac3016a34f0c38d65dc0ba
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2018-04-11 23:08:05 -0700

    added GET /todos test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,21 +4,35 @@ const app = require('../server').app
 const Todo = require('../models/todo').Todo
 const User = require('../models/user').User
 
+//creating dummy todos for testing GET /todos
+const todos = [{
+    text: 'First test todo'
+},{
+    text: 'Second test todo'
+}]
+
 //deleting all docs from Todo before running
 beforeEach((done) => {
-    Todo.remove({}).then((success) => {
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos, (err, docs) => {
+            if (err){
+                return done(err)
+            }
+        })
+    }).then(() => {
         done()
-    }, (err) => {
-        console.log(err)
+    }).catch((err) => {
+        done(err)
     })
 })
 
 describe('POST /todos', () => {
+    
 
     let text = 'Test todo'
 
     //sending a post request to app to save doc. 
-    //If the db doesn't have exactly one doc in it then there is an error
+    //If the db doesn't have exactly one doc with the ascribed text then there is an error
     //if db has a single doc then it's text field must be the doc's text
     it('Responds with saved todo', (done) => {
         request(app)
@@ -35,7 +49,7 @@ describe('POST /todos', () => {
                     done(err)
                     return
                 }
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1)
                     expect(todos[0].text).toBe(text)
                     done()
@@ -45,7 +59,7 @@ describe('POST /todos', () => {
             })
     })
 
-    //sending invalid data and testing if no doc is stored in todos db which is the desired behavior
+    //sending invalid data and testing if no doc is stored in todos db which is the desired behavior, not there are 2 dummy todos
     it('Should not create todo with invalid body data', (done) => {
         request(app)
             .post('/todos')
@@ -59,7 +73,7 @@ describe('POST /todos', () => {
                 }
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0)
+                    expect(todos.length).toBe(2)
                     done()
                 }, (err) => {
                     done(err)
@@ -67,4 +81,33 @@ describe('POST /todos', () => {
 
             })
     })
+})
+
+//testing GET, should only return 2 docs whose text are equivalent to the dummy todos
+describe('GET /todos', () => {
+
+    it('Should only return dummy todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) =>{
+                expect(res.body.length).toBe(2)
+            })
+            .end((err,res) => {
+                
+                if (err){
+                    done(err)
+                    return
+                }
+                //checking only dummy todos exist
+                Todo.find().then((todos) => {
+                    expect(todos[0].text).toBe('First test todo')
+                    expect(todos[1].text).toBe('Second test todo')
+                    done()
+                }, (err) => {
+                    done(err)
+                })
+            })      
+    })
+
 })
