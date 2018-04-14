ae9e3672b576b489aaf1129d9f84b5f3aa9d02d2
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2018-04-14 04:47:18 -0700

    refactored process.env.NODE_ENV and process.env.MONGODB_URI setting into config/config.js and I import it into server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,4 @@
-//by changing env we can differentiate between production,development and test databases
-//note that heroku sets node_env to production and MONGODB_URI and by default node_env is undefined
-let env = process.env.NODE_ENV || 'development'
-
-if (env === 'development'){
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-}
-else if (env === 'test'){
-    process.env.PORT = 3000
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
-}
+require('./config/config')
 
 const express = require('express')
 const bodyParser = require('body-parser')
