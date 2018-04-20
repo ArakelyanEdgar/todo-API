e24db533ccdb3b90bc197f36798996ddb5f70636
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2018-04-20 14:59:16 -0700

    a

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,20 @@
 //by changing env we can differentiate between production,development and test databases
 //note that heroku sets node_env to production and MONGODB_URI and by default node_env is undefined
 //note that test is activated on the test script in npm test seen in package.json
+
 let env = process.env.NODE_ENV || 'development'
+let config = require('./config.json')
 
 //for local databases
 if (env === 'development' || env === 'test'){
-    //requiring json files parses them automatically
-    let config = require('./config.json')
     let envConfig = config[env]
     Object.keys(envConfig).forEach((key) => {
         process.env[key] = envConfig[key]
     })
 }
 
-//for production databases I set heroku:config jwt_secret=....
-
+//for production databases note that PORT and mongodb_uri are set by HEROKU
+if (env === 'production'){
+    process.env.JWT_SECRET = config.production.JWT_SECRET
+}
 
