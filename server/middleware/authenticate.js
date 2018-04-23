dba02fa40eac282ef8126e7aa5a5ad1325c41191
Edgar Arakelyan <arakee1@unlv.nevada.edu>
2018-04-22 18:03:56 -0700

    changed authenticate.js middleware to async/await syntax

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,24 +1,23 @@
 const User = require('../models/user').User
 
 //middleware that authenticates user and assigns user to req.user
-let authenticate = (req, res, next) => {
+let authenticate = async (req, res, next) => {
     let token = req.cookies['x-auth']
     //if token doesn't exist then return 401 for unauthorized user
     if(!token){
         res.status(401).send()
         return
     }
-    User.findByToken(token).then(user => {
-        if (user === null){
-            next()
+    try{
+        let user = await User.findByToken(token)
+        if (!user){
+            throw new Error('User not found')
         }
-
-        //attach user to req
         req.user = user
         next()
-    }).catch(err => {
-        next()
-    })
+    }catch(err){
+        res.status(404).send()
+    }
 }
 
 module.exports = {
